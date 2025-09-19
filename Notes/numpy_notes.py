f35b52e26d4e3595aaf38ed00fda69d47b0a258d
abinash <dasabinash1807@gmail.com>
2025-09-19 11:00:01 +0530

    statistics file  added

---
+++ Notes/numpy_notes.py
@@ -0,0 +1,161 @@
+import numpy as np
+arr = np.array([2, 4, 5, 6, 7])
+arr
+import numpy as np
+arr = np.array([2, 4, 5, 6, 7])
+print(arr)
+print(arr.sort())
+print(arr.sort)
+print(anp.sort(arr))
+print(np.sort(arr))
+import numpy as np
+arr = np.array([2, 4, 5, 6, 7, 3])
+print(arr)
+print(np.sort(arr))
+print(np.argsort(arr))
+print(np.ndarray.sort(arr))
+print(arr.ndarray.sort())
+print(np.ndarray.sort(arr))
+print(np.ndarray.sort())
+print(np.ndarray.sort(arr))
+print(np.sort(arr))[::-1]
+print(np.sort(arr)[::-1])
+arr.sort()
+arr.sort()
+print(arr)
+# Sort 2D array
+arr = np.array([[12,11,15],[21,25,20],[18,19,28]])
+print(arr)
+# Sort 2D array
+arr = np.array([[12,11,15],[21,25,20],[18,19,28]])
+print(arr)
+print("Sorted array")
+# Sort 2D array
+arr = np.array([[12,11,15],[21,25,20],[18,19,28]])
+print(arr)
+print("Sorted array")
+y = np.sort(arr)
+print(y)
+y = np.sort(arr, axis = 0)   #column wise sorting
+y = np.sort(arr, axis = 0)   #column wise sorting
+print(y)
+arr1 = np.array([[[13,12,4],[21,32,43]],[[11,21,32],[22,21,13]]])
+print(arr1)
+print(np.sort(arr1))
+print(np.sort(arr1, axis = ))
+print(np.sort(arr1, axis = 0))
+# Addition 
+x = np.arraay([[1,2],[3,4]])
+y = np.arrray([[11,12],[13,14]])
+print(x, y)
+# Addition 
+x = np.array([[1,2],[3,4]])
+y = np.array([[11,12],[13,14]])
+print(x, y)
+# Addition 
+x = np.array([[1,2],[3,4]])
+y = np.array([[11,12],[13,14]])
+print(x,\n y)
+# Addition 
+x = np.array([[1,2],[3,4]])
+y = np.array([[11,12],[13,14]])
+print(x,"\n", y)
+a = x + y
+b = x - y
+c = x * y
+d = x // y
+e = x / y
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+a = x + y
+b = x - y
+c = x * y
+d = y // x
+e = x / y
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+a = x + y
+b = x - y
+c = x * y
+d = y // x
+e =  round((x / y), 2)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+a = x + y
+b = x - y
+c = x * y
+d = y // x
+e =  np.round((x / y), 2)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+# Matrix Multiplication
+z = x @ y
+print(z)
+z = np.dot(x,y)
+z = np.dot(x,y)
+print(z)
+z = np.transpose(x)
+print(z)
+print(x.T)
+x = np.arange(0,11)
+x = np.arange(0,11)
+print(x)
+for i in x:
+    print(i)
+# Iterating in 2D arrray 
+x = np.arange(0,12)
+# Iterating in 2D arrray 
+x = np.arange(0,12).reshape(4,3)
+# Iterating in 2D arrray 
+x = np.arange(0,12).reshape(4,3)
+print(x)
+for i in x:
+    print(i)
+for i in x:
+    for j in i:
+for i in x:
+    for j in i:
+        print(j)
+x = np.arange(12).reshape(2,2,3)
+print(x)
+for i in x:
+    for j in i:
+        for m in j:
+            print(m)
+# np.nditer()
+print(np.nditer(x))
+print(x.nditer)
+y = np.nditer(x)
+y = np.nditer(x)
+print(y)
+for i in np.nditer():
+for i in np.nditer():
+    print(I)
+for i in np.nditer(x):
+    print(I)
+for i in np.nditer(x):
+    print(i)
+print(x.reshape(1))
+print(x.reshape(-1))
+arr = np.where(x > 5)
+print(arr)
+print(x > 5)
+print(arr > 5)
+y = x.reshape(-1)
+print(y)
+arr = np.where(y > 5)
+print(arr)
+print(y > 5)
+%history -f numpy.py
